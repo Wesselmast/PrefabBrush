77e79e5349f9a69274ec32d5ecc3c9f63648d268
Wesselmast <mastwessel@gmail.com>
2019-04-05 15:38:47 +0200

    Random rotation and parenting

--- Assets/PrefabBrush.cs
+++ Assets/PrefabBrush.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 public class PrefabBrush : MonoBehaviour {
+
+    [SerializeField] private GameObject targetGround = null;
+    [SerializeField] private bool enableBrush = false;
+
+    private GameObject parent = null;
+
     private void OnEnable() {
         if (!Application.isEditor) {
             Destroy(this);
         }
+        enableBrush = false;
         SceneView.onSceneGUIDelegate += OnScene;
     }
 
@@ -14,18 +21,24 @@ public class PrefabBrush : MonoBehaviour {
         SceneView.onSceneGUIDelegate -= OnScene;
     }
 
-    private static void OnScene(SceneView scene) {
+    private void OnScene(SceneView scene) {
+        if (!enableBrush) return;
         if (Event.current.keyCode == KeyCode.Q) {
             Vector3 mousePosition = Event.current.mousePosition;
             float ppp = EditorGUIUtility.pixelsPerPoint;
             mousePosition.y = scene.camera.pixelHeight - mousePosition.y * ppp;
             mousePosition.x *= ppp;
             if (Physics.Raycast(scene.camera.ScreenPointToRay(mousePosition), out RaycastHit hit)) {
+                if (hit.collider.gameObject != targetGround) return;
+                if (parent == null) {
+                    parent = new GameObject("Parent");
+                }
                 GameObject prefab = (GameObject)Resources.Load("Example", typeof(GameObject));
                 GameObject prefabInstance = ((GameObject)PrefabUtility.InstantiatePrefab(prefab));
                 prefabInstance.transform.position = hit.point;
+                prefabInstance.transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
+                prefabInstance.transform.parent = parent.transform;
             }
-            Event.current.Use();
         }
     }
 }
