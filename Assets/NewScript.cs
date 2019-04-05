1d6ebb7f733a305584f52a54db78ae8b9dd2587e
Wesselmast <mastwessel@gmail.com>
2019-04-05 14:38:38 +0200

    Init

---
+++ Assets/NewScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Leeg))]
+public class NewScript : Editor {
+    private Vector3 position;
+    public Vector3 Position {
+        get {
+            return position;
+        }
+    }
+
+    private void OnSceneGUI() {
+        Tools.current = Tool.None;
+        if (Event.current == null) return;
+        position = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(Event.current.mousePosition);
+
+        if (!Event.current.Equals(Event.KeyboardEvent("return"))) return;
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = position;
+    }
+}
---
+++ Assets/Placement/DPlacementGUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DPlacementGUI : EditorWindow {
+    public GameObject objectToPlace;
+    public static bool shouldPlaceDecall;
+
+    [MenuItem("Window/Decal Placement")]
+    public static void ShowWindow() {
+        DPlacementGUI window = GetWindow<DPlacementGUI>("Decall Placement");
+        window.minSize = new Vector2(200, 200);
+    }
+
+    private void OnGUI() {
+        GUILayout.Label("placement", EditorStyles.boldLabel);
+        GUILayout.Label("Scale", EditorStyles.foldout);
+        GUILayout.Label("My Cube Prefab");
+        objectToPlace = EditorGUILayout.ObjectField(objectToPlace, typeof(GameObject), true) as GameObject;
+
+    }/*
+
+        if (Decall.paintMode == true) {
+            if (GUILayout.Button("Disable")) {
+                Decall.paintMode = false;
+                Debug.Log("Disabled Placement Mode");
+            }
+        }
+        else {
+            if (GUILayout.Button("Enable")) {
+                Decall.paintMode = true;
+                Debug.Log("Enabled Placement Mode");
+            }
+        }
+
+        if (GUILayout.Button("Gerrit")) {
+            Decall.Gerrit();
+        }
+    }
+
+    void Update() {
+        if (Decall.paintMode == true) {
+            Tools.current = Tool.None;
+        }
+
+        if (Decall.paintMode == true && Decall.Click == true) {
+            Decall.PlaceDecall(objectToPlace);
+            Repaint();
+            Decall.Click = false;
+        }
+    }
+}*/
+}
