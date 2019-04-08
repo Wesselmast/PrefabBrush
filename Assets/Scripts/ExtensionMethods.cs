da4a175810818c38b51d4a2c704353b32e7a800f
Wesselmast <mastwessel@gmail.com>
2019-04-08 16:05:19 +0200

    Added brush size and gizmos

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods {
+    public static Vector3 SetX(this Vector3 vector3, float x) {
+        return new Vector3(x, vector3.y, vector3.z);
+    }
+    public static Vector3 SetY(this Vector3 vector3, float y) {
+        return new Vector3(vector3.x, y, vector3.z);
+    }
+    public static Vector3 SetZ(this Vector3 vector3, float z) {
+        return new Vector3(vector3.x, vector3.y, z);
+    }
+}
--- Assets/Scripts/PrefabBrush.cs
+++ Assets/Scripts/PrefabBrush.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 public class PrefabBrush : MonoBehaviour {
+    [SerializeField] private float brushSize = 5f;
+
     public GameObject targetGround = null;
     [HideInInspector] public bool enableBrush = false;
     [HideInInspector] public List<GameObject> meshList;
     private GameObject parent = null;
+
+    private Vector3 hitPoint = Vector3.zero;
 
     private void OnEnable() {
         if (!Application.isEditor) {
@@ -22,27 +26,53 @@ public class PrefabBrush : MonoBehaviour {
     }
 
     private void OnScene(SceneView scene) {
-        if (!enableBrush) return;
-        Tools.current = Tool.None;
-        if (Event.current.type == EventType.MouseDown && Event.current.button == 0) {
-            Vector3 mousePosition = Event.current.mousePosition;
-            float ppp = EditorGUIUtility.pixelsPerPoint;
-            mousePosition.y = scene.camera.pixelHeight - mousePosition.y * ppp;
-            mousePosition.x *= ppp;
-            if (Physics.Raycast(scene.camera.ScreenPointToRay(mousePosition), out RaycastHit hit)) {
-                if (hit.collider.gameObject != targetGround) return;
-                if (parent == null) {
-                    parent = new GameObject("Parent");
-                }
-                GameObject prefab = (GameObject)Resources.Load("Example", typeof(GameObject));
-                GameObject prefabInstance = ((GameObject)PrefabUtility.InstantiatePrefab(prefab));
-                prefabInstance.transform.position = hit.point;
-                prefabInstance.transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
-                prefabInstance.transform.parent = parent.transform;
-            }
+        Event e = Event.current;
+        int controlID = GUIUtility.GetControlID(FocusType.Keyboard);
+
+        if (e.GetTypeForControl(controlID) == EventType.KeyDown && e.keyCode == KeyCode.B) {
+            enableBrush = !enableBrush;
+            if (enableBrush) Debug.Log("Brush mode on");
+            else Debug.Log("Brush mode off");
         }
+
+        if (!enableBrush) return;
+
+        HandleUtility.AddDefaultControl(controlID);
+        Tools.current = Tool.None;
+
+        if (!((e.type == EventType.MouseDown || e.type == EventType.MouseDrag) && e.button == 0)) return;
+
+        Vector3 mousePosition = e.mousePosition;
+        float ppp = EditorGUIUtility.pixelsPerPoint;
+        mousePosition.y = scene.camera.pixelHeight - mousePosition.y * ppp;
+        mousePosition.x *= ppp;
+        if (Physics.Raycast(scene.camera.ScreenPointToRay(mousePosition), out RaycastHit hit)) {
+            if (hit.collider.gameObject != targetGround) return;
+            if (parent == null) parent = new GameObject("BrushedItems");
+            GameObject prefab = (GameObject)Resources.Load("Example", typeof(GameObject));
+            GameObject prefabInstance = ((GameObject)PrefabUtility.InstantiatePrefab(prefab));
+
+            hitPoint = hit.point.SetY(hit.point.y + prefabInstance.transform.position.y);
+
+            float randomNum = Random.Range(0f, 1f); 
+            float circumfrence = randomNum * 2 * Mathf.PI;
+            float radius = brushSize * Mathf.Sqrt(randomNum);
+            float randomX = hitPoint.x + radius * Mathf.Cos(circumfrence);
+            float randomY = hitPoint.z + radius * Mathf.Sin(circumfrence);
+            Vector3 randomPosition = new Vector3(randomX, 0, randomY);
+            randomPosition = randomPosition.SetY(Terrain.activeTerrain.SampleHeight(randomPosition) + prefabInstance.transform.position.y);
+
+            prefabInstance.transform.position = randomPosition;
+            prefabInstance.transform.eulerAngles = prefabInstance.transform.eulerAngles.SetY(Random.Range(0, 360));
+            prefabInstance.transform.parent = parent.transform;
+        }
+        Event.current.Use();
     }
 
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(hitPoint, brushSize);
+    }
 
     public void AddMesh(GameObject mesh) {
         meshList.Add(mesh);
