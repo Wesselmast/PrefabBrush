47751ee74853636c48490790837e64d120e977aa
Wesselmast <mastwessel@gmail.com>
2019-04-12 12:55:45 +0200

    Cleaned up a bit, weird warning on using events but rest is cool

--- Assets/Scripts/PrefabBrush.cs
+++ Assets/Scripts/PrefabBrush.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PrefabBrush : MonoBehaviour {
     [SerializeField] private float brushSize = 5f;
     [SerializeField] private float minimumDistance = 1f;
-    [SerializeField] private float scaleFactor = 1.5f;
+    [SerializeField] private Vector2 scale = new Vector2(.5f, 1.5f);
     [SerializeField] private float spawnDelay = 1f;
     [SerializeField] private int prefabDensity = 3;
     [SerializeField] private bool eraserOn = false;
@@ -15,10 +15,10 @@ public class PrefabBrush : MonoBehaviour {
 
     public GameObject targetGround = null;
     [HideInInspector] public bool enableBrush = false;
-    [HideInInspector] public List<GameObject> meshList = new List<GameObject>();
+    //[HideInInspector] public List<GameObject> meshList = new List<GameObject>();
     private GameObject parent = null;
 
-    private List<GameObject> meshCollection;
+    private List<GameObject> meshCollection = new List<GameObject>();
 
     private Vector3 hitPoint = Vector3.zero;
     private float elapsed = 0;
@@ -28,6 +28,7 @@ public class PrefabBrush : MonoBehaviour {
             Destroy(this);
         }
         enableBrush = false;
+        eraserOn = false;
         SceneView.onSceneGUIDelegate += OnScene;
     }
 
@@ -45,18 +46,16 @@ public class PrefabBrush : MonoBehaviour {
             else Debug.Log("Brush mode off");
         }
 
-        if (e.GetTypeForControl(controlID) == EventType.KeyDown && e.keyCode == KeyCode.E && enableBrush) {
-            eraserOn = !eraserOn;
-            if (enableBrush) Debug.Log("Eraser mode on");
-            else Debug.Log("Eraser mode off");
-        }
-
         if (!enableBrush) return;
 
         HandleUtility.AddDefaultControl(controlID);
         Tools.current = Tool.None;
 
-        if (!((e.type == EventType.MouseDown || e.type == EventType.MouseDrag) && e.button == 0)) return;
+        if (e.GetTypeForControl(controlID) == EventType.KeyDown && e.keyCode == KeyCode.E) {
+            eraserOn = !eraserOn;
+            if (enableBrush) Debug.Log("Eraser mode on");
+            else Debug.Log("Eraser mode off");
+        }
 
         Vector3 mousePosition = e.mousePosition;
         float ppp = EditorGUIUtility.pixelsPerPoint;
@@ -64,9 +63,14 @@ public class PrefabBrush : MonoBehaviour {
         mousePosition.x *= ppp;
 
         if (Physics.Raycast(scene.camera.ScreenPointToRay(mousePosition), out RaycastHit hit)) {
+            hitPoint = hit.point;
+
             if (hit.collider.gameObject != targetGround) return;
 
-            hitPoint = hit.point;
+            if (!((e.type == EventType.MouseDown || e.type == EventType.MouseDrag) && e.button == 0)) {
+                e.Use();
+                return;
+            }
 
             if(eraserOn) {
                 foreach (GameObject go in meshCollection.ToArray()) {
@@ -75,7 +79,7 @@ public class PrefabBrush : MonoBehaviour {
                         DestroyImmediate(go);
                     }
                 }
-                Event.current.Use();
+                e.Use();
                 return;
             }
 
@@ -86,40 +90,41 @@ public class PrefabBrush : MonoBehaviour {
             }
             if (elapsed < spawnDelay && e.type == EventType.MouseDrag) {
                 elapsed += Time.deltaTime;
+                e.Use();
+                return;
             }
-            else {
-                for (int i = 0; i < prefabDensity; i++) {
-                    GameObject prefabInstance = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+            for (int i = 0; i < prefabDensity; i++) {
+                GameObject prefabInstance = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
 
-                    Vector3 randomPosition = hitPoint + Random.insideUnitSphere * brushSize;
-                    randomPosition = randomPosition.SetY(Terrain.activeTerrain.SampleHeight(randomPosition) + prefabInstance.transform.position.y);
+                Vector3 randomPosition = hitPoint + Random.insideUnitSphere * brushSize;
+                randomPosition = randomPosition.SetY(Terrain.activeTerrain.SampleHeight(randomPosition) + prefabInstance.transform.position.y);
 
-                    prefabInstance.transform.position = randomPosition;
-                    prefabInstance.transform.eulerAngles = prefabInstance.transform.eulerAngles.SetY(Random.Range(0, 360));
+                prefabInstance.transform.position = randomPosition;
+                prefabInstance.transform.eulerAngles = prefabInstance.transform.eulerAngles.SetY(Random.Range(0, 360));
 
-                    prefabInstance.transform.localScale *= Random.Range(.5f, 1.5f) * scaleFactor;
-                    prefabInstance.transform.parent = parent.transform;
+                prefabInstance.transform.localScale *= Random.Range(scale.x, scale.y);
+                prefabInstance.transform.parent = parent.transform;
 
-                    foreach (GameObject go in meshCollection) {
-                        if (Vector3.Distance(go.transform.position, prefabInstance.transform.position) < minimumDistance) {
-                            DestroyImmediate(prefabInstance);
-                            break;
-                        }
+                foreach (GameObject go in meshCollection) {
+                    if (Vector3.Distance(go.transform.position, prefabInstance.transform.position) < minimumDistance) {
+                        DestroyImmediate(prefabInstance);
+                        break;
                     }
-                    if (prefabInstance != null) meshCollection.Add(prefabInstance);
                 }
-                elapsed = 0;
+                if (prefabInstance != null) meshCollection.Add(prefabInstance);
             }
+            elapsed = 0;
         }
-        Event.current.Use();
     }
 
     private void OnDrawGizmos() {
-        Gizmos.color = Color.white;
+        if (!enableBrush) return;
+        if (eraserOn) Gizmos.color = Color.red;
+        else Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(hitPoint, brushSize);
     }
 
-    public void AddMesh(GameObject mesh) {
-        meshList.Add(mesh);
-    }
+   // public void AddMesh(GameObject mesh) {
+    //    meshList.Add(mesh);
+    //}
 }
